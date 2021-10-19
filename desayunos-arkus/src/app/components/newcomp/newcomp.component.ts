fd449f414175c835695a7fb22ac83013fc31d4eb
ivanalejandrogp <ivanalejandro.gpz@outlook.com>
2021-10-19 12:50:33 -0500

    Inicio de Calendario

--- desayunos-arkus/src/app/components/newcomp/newcomp.component.ts
+++ desayunos-arkus/src/app/components/newcomp/newcomp.component.ts
@@ -10,6 +10,10 @@ export class NewcompComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const calendar = document.querySelector("#app-calendar");
+    for(let day = 1; day <= 31; day++){
+      calendar?.insertAdjacentHTML("beforeend",`<div class="day">${day}</div>`);
+    }
   }
 
 }
