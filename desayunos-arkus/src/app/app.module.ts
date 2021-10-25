7e5b0d89d24a05680940c7ddc35efe1c217a7cf6
ivanalejandrogp <ivanalejandro.gpz@outlook.com>
2021-10-25 11:17:20 -0500

    Se agregó el calendario con funcionalidad

--- desayunos-arkus/src/app/app.module.ts
+++ desayunos-arkus/src/app/app.module.ts
@@ -3,12 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewcompComponent } from './components/newcomp/newcomp.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    NewcompComponent
   ],
   imports: [
     BrowserModule,
