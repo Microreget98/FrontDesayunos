25dfecc660c18f6fac3bc5b367aa9afa9363f3de
ivanalejandrogp <ivanalejandro.gpz@outlook.com>
2021-10-29 12:49:27 -0500

    Agregamos tipografia global

--- desayunos-arkus/src/app/components/home/home.component.ts
+++ desayunos-arkus/src/app/components/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent implements OnInit {
         title: "Evento1",
         start: new Date(),
         description: "Evento1",
-        allDay: true
+        allDay: true,
       },
       {
         title: "Evento2",
