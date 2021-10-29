d4ce50ee8779b4331b4f6f2477e8a6fdd5431ced
ivanalejandrogp <ivanalejandro.gpz@outlook.com>
2021-10-29 11:04:23 -0500

    Elementos del Calendario

--- desayunos-arkus/src/app/components/home/home.component.ts
+++ desayunos-arkus/src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 
 @Component({
   selector: 'app-home',
@@ -8,15 +12,38 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public events: any[];
-  public options: any[];
+  public options: any;
   constructor() { }
 
   ngOnInit(): void {
+
+    this.options = {
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      defaultDate: new Date(),
+      header:{
+        left: 'prev, next',
+        center: 'title',
+        right: 'dayGridMonth, timeGridweek, timeGridDay'
+      },
+      editable: false
+    }
+
     this.events = [
       {
         title: "Evento1",
         start: new Date(),
         description: "Evento1"
+      },
+      {
+        title: "Evento2",
+        start: new Date( new Date().getTime() + 86400000 ),
+        description: "Evento2"
+      },
+      {
+        title: "Evento3",
+        start: new Date( new Date().getTime() + (86400000 * 2) ),
+        end: new Date( new Date().getTime() + (86400000 * 3) ),
+        description: "Evento3"
       }
     ]
   }
