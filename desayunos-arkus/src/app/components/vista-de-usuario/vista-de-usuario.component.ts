513db287063294e1966e4cadb2e5309314f2c7cd
LilyMedina14 <90793026+LilyMedina14@users.noreply.github.com>
2021-11-18 12:39:41 -0600

    WIP: vista de lista de usuarios

--- desayunos-arkus/src/app/components/vista-de-usuario/vista-de-usuario.component.ts
+++ desayunos-arkus/src/app/components/vista-de-usuario/vista-de-usuario.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {LiveAnnouncer} from '@angular/cdk/a11y';
-import {AfterViewInit, ViewChild} from '@angular/core';
-import {MatSort, Sort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+
 
 export interface PeriodicElement {
   
@@ -18,28 +16,9 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class VistaDeUsuarioComponent implements OnInit {
  
-    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-    dataSource = new MatTableDataSource(ELEMENT_DATA);
   
     constructor(private _liveAnnouncer: LiveAnnouncer) {}
-  
-    @ViewChild(MatSort)
-    sort: MatSort = new MatSort;
-  
-    ngAfterViewInit() {
-      this.dataSource.sort = this.sort;
-    }
-    announceSortChange(sortState: Sort) {
-      // This example uses English messages. If your application supports
-      // multiple language, you would internationalize these strings.
-      // Furthermore, you can customize the message to add additional
-      // details about the values being sorted.
-      if (sortState.direction) {
-        this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-      } else {
-        this._liveAnnouncer.announce('Sorting cleared');
-      }
-    }
+
   ngOnInit(): void {
   }
 
