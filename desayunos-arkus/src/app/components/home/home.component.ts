8c53fa5da80696fb90f8252f89dcfe708ffd4882
Francisco <fac.sod@gmail.com>
2021-11-22 11:21:57 -0600

    Changes for showing users in calendar

--- desayunos-arkus/src/app/components/home/home.component.ts
+++ desayunos-arkus/src/app/components/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { CalendarOptions, createElement, FullCalendarComponent } from '@fullcalendar/angular';
 import { ApiService } from '../../core/api.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogMenu } from './DialogMenu/dialog-menu.component';
 import { UserDataService } from '../login-registro/user-data.service';
+import { Calendar } from '@fullcalendar/core';
+import { ConfigService } from 'src/app/core/config.service';
+import { map } from 'rxjs/operators';
+import { CalendarUsersByMonth } from './models/CalendarUsersByMonth';
 
+const eventLoad = [];
 
 @Component({
   selector: 'app-home',
@@ -12,6 +17,11 @@ import { UserDataService } from '../login-registro/user-data.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  isLoading: boolean = true;
+
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
+  
   userInfo = {
     id_user: this.userData.userData[0].id_user,
     first_name: this.userData.userData[0].first_name,
@@ -33,18 +43,76 @@ export class HomeComponent implements OnInit {
       right: ''
     },
     initialView: 'dayGridMonth',
+    initialEvents: eventLoad,
     weekends: true,
     editable: true,
     selectable: true,
     selectMirror: true,
     dayMaxEvents: true,
     dateClick: this.handleDateClick.bind(this),
+    eventContent: function(arg){
+      let image = document.createElement('img');
+      let div = document.createElement('span');
+      image.src = arg.event.extendedProps.imgUrl;
+      image.width = 20;
+      image.height = 20;
+      div.innerText = arg.event.extendedProps.fullName;
+      let arrayOfDomNodes = [ image, div ];
+      return { domNodes: arrayOfDomNodes };
+    },
+    events: [
+      eventLoad
+    ]
   };
 
-  constructor(public dialog: MatDialog, private userData: UserDataService) {  }
+  constructor(
+    public dialog: MatDialog,
+    private userData: UserDataService,
+    private apiService: ApiService,
+    private configService: ConfigService
+    ) {
+
+      
+      }
 
   ngOnInit(): void {
+
+    this.loadCalendarInfo();
+    this.options.dayMaxEvents = 4;
     
+  }
+
+  loadCalendarInfo(){
+    let calendarEl;
+      var calendar = new Calendar(calendarEl, {
+        timeZone: 'local',
+      });
+      let month: number = calendar.view.currentStart.getMonth() + 1;
+      let year: number = calendar.view.currentStart.getFullYear();
+
+      const apiUrl = `${this.configService.config.apiUrl}/api/Calendar/GetRegisterUsersByMonth?month=${month}&year=${year}`
+
+      this.apiService.GetData(apiUrl).pipe(
+        map((res: CalendarUsersByMonth[]) => {
+          console.log(res);
+          res.forEach(usr => {
+            eventLoad.push(
+              { 
+                title: 'user', 
+                date: usr.date ,
+                backgroundColor: 'white', 
+                textColor: 'black',
+                borderColor: 'white',
+                extendedProps: {
+                  fullName: usr.first_name + " " + usr.last_name,
+                  imgUrl: "../../../assets/img/avatar.jpg"
+                }
+              }
+            );
+          });
+          this.isLoading = false;
+        })
+      ).subscribe();
   }
 
   handleDateClick(info){
@@ -53,6 +121,8 @@ export class HomeComponent implements OnInit {
       width: '800px',
       height: '500px',
       data: {dateStr: info.dateStr, firstName: this.userInfo.first_name, lastName: this.userInfo.last_name, userId: this.userInfo.id_user}
+    }).afterClosed().subscribe((res) => {
+      this.loadCalendarInfo();
     });
   }
 
