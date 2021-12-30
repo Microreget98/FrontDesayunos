caa4fa7908f8c64c21835d8c3beec123d5b815bb
Dante Barboza <90792603+DanteCoder@users.noreply.github.com>
2021-12-30 12:12:22 -0600

    calendar functions and style
    created getDateSpan function,  added padding on main class, and added a calendar-day component as placeholder

--- desayunos-arkus/src/app/components/calendar/calendar.component.ts
+++ desayunos-arkus/src/app/components/calendar/calendar.component.ts
@@ -8,7 +8,28 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CalendarComponent implements OnInit {
   @Input() monthBreakFasts: any;
 
+  year: number = 2022;
+  // January is 0 based
+  month: number = 3;
+
+  dateSpan: Array<Date>;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.dateSpan = this.getDateSpan(this.month, this.year);
+    console.log('datespan', this.dateSpan);
+  }
+
+  getDateSpan(month: number, year: number) {
+    const dateSpan = [];
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const firstWeekDay = new Date(year, month, 1).getDay();
+    const lastWeekDay = new Date(year, month, daysInMonth).getDay();
+
+    dateSpan.push(new Date(year, month, 2 - firstWeekDay));
+    dateSpan.push(new Date(year, month, daysInMonth + 5 - lastWeekDay));
+
+    return dateSpan;
+  }
 }
