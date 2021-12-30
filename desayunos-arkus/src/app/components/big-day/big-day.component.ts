50590cf334bd4234e7cbe19e2c84f973c366111b
Steve Rodriguez <90792719+SteveRodriguezArkNexus@users.noreply.github.com>
2021-12-30 12:01:37 -0600

    bigDay component done

--- desayunos-arkus/src/app/components/big-day/big-day.component.ts
+++ desayunos-arkus/src/app/components/big-day/big-day.component.ts
@@ -3,18 +3,18 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-big-day',
   templateUrl: './big-day.component.html',
-  styleUrls: ['./big-day.component.scss']
+  styleUrls: ['./big-day.component.scss'],
 })
 export class BigDayComponent implements OnInit {
-@Input() date: Date =new Date();
-dayNumber:number;
-dayName:number;
-  constructor() { }
+  @Input() date: Date = new Date();
+  dayNumber: number;
+  weekDayName: string;
+  constructor() {}
 
   ngOnInit(): void {
-  this.dayNumber = this.date.getDate();
-  console.log(this.date)
-  this.dayName = this.date.getDay();
+    this.dayNumber = this.date.getDate();
+    this.weekDayName = this.date.toLocaleDateString('es-MX', {
+      weekday: 'long',
+    });
   }
-
 }
